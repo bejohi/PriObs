22b44cec1dd309ba3142278f02231b230e532ac3
Jonas Hielscher <benedikt.hielscher@st.ovgu.de>
2017-07-01 21:35:35 +0200

    Added the webloader.go file

--- crawler/webloader.go
+++ crawler/webloader.go
@@ -1,1 +1,17 @@
 package crawler
+
+import (
+	"golang.org/x/net/html"
+	"net/http"
+)
+
+// Loads a website from the network and returns the root of the html tree.
+// Returns an error if the connection failed,
+// or if the received html document was not valid formed.
+func LoadWebRoot(url string) (*html.Node,error){
+	website, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	return html.Parse(website.Body)
+}
